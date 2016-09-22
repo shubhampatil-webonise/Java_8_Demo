bd6a3b4892e20366690ad8616cbf6ad9a426900a
shubhampatil-webonise <shubham.patil@weboniselab.com>
2016-09-22 17:54:42 +0530

    implement lambdas for prettyPrintApple method

---
+++ src/org/webonise/lambdafundamentals/Apple.java
@@ -0,0 +1,21 @@
+package org.webonise.lambdafundamentals;
+
+public class Apple {
+    private final int weight;
+    private final String color;
+
+    Apple(int weight, String color) {
+        this.weight = weight;
+        this.color = color;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
+
+
